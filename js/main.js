648095e72a92befc7b4b126e8f9afbced99f7ea4
risuinazoo <353226714@qq.com>
2022-12-28 18:48:22 +0800

    Site updated: 2022-12-28 18:48:22

--- js/main.js
+++ js/main.js
@@ -1,34 +1,117 @@
-$(function(){
-	secondary = $( '#secondary' );
-	button = $( '.site-branding' ).find( '.secondary-toggle' );
+(function () {
+  var searchEl = document.querySelector('.search-do'),
+      searchWrap = document.querySelector('.search-wrap'),
+      searchContent = document.querySelector('.search-content'),
+      inputEl = searchWrap.querySelector('input'),
+      resultEl = searchWrap.querySelector('.search-result'),
+      oldResultElString = resultEl.innerHTML,
+      postData;
+  
+  function showSearch (e) {
+    e.preventDefault();
+    searchWrap.style.display = 'flex';
+    inputEl.focus();
+    getSearchData();
+  }
 
-	// Enable menu toggle for small screens.
-	( function() {
-		var menu, widgets, social;
-		if ( ! secondary || ! button ) {
-			return;
-		}
+  function hideSearch () {
+    searchWrap.style.display = 'none';
+    resultEl.innerHTML = oldResultElString;
+    postData = null;
+  }
 
-		button.on( 'click', function() {
-			secondary.toggleClass( 'toggled-on' );
-			secondary.trigger( 'resize' );
-			$( this ).toggleClass( 'toggled-on' );
-			if ( $( this, secondary ).hasClass( 'toggled-on' ) ) {
-				$( this ).attr( 'aria-expanded', 'true' );
-				secondary.attr( 'aria-expanded', 'true' );
-			} else {
-				$( this ).attr( 'aria-expanded', 'false' );
-				secondary.attr( 'aria-expanded', 'false' );
-			}
-		} );
-	} )();
-	
-	footer = $( '#infinite-footer' );
-	setInterval( function() {
-			// Reveal footer
-		if ( $(window).scrollTop() >= 350 )
-			footer.animate( { 'bottom': 0 }, 'fast' );
-		else if ( $(window).scrollTop() < 350 )
-			footer.animate( { 'bottom': '-50px' }, 'fast' );
-		}, 250 );
-})
+  function indexOfIgnoreTransform (a, b) {
+    a = (a || '').toLocaleLowerCase();
+    b = (b || '').toLocaleLowerCase();
+
+    return a.indexOf(b) > -1;
+  }
+
+  function searchFromJson(data, key) {
+    if (!data || data.length < 1 || !key) {
+      return;
+    }
+    return data.filter(function(post) {
+      if (indexOfIgnoreTransform(post.title, key)) {
+        return true;
+      }
+      if (indexOfIgnoreTransform(post.content, key)) {
+        return true;
+      }
+    })
+  }
+
+  function render(data) {
+    if (!data || data.length < 1) {
+      return resultEl.innerHTML = '<div class="search-no">NOT FIND!</div>';
+    }
+    var htmlString = '',
+      tempEl = document.createElement('div');
+
+    data.forEach(function(post) {
+      tempEl.innerHTML = (post.content || '').slice(0, 100)
+      htmlString += '<li data-url="' + post.url + '"' + '><h3>' + 
+        post.title + '</h3><p>' + tempEl.innerText + '</p></li>'
+    });
+
+    resultEl.innerHTML = htmlString;
+  }
+
+  function getSearchData (callback) {
+    if (postData) {
+      callback && callback(postData);
+      return;
+    }
+    fetch('/search.json').then(function (respons) {
+      respons.json().then(function(data){
+        postData = data;
+        callback && callback(postData);
+      }); 
+    }).catch(function() {
+      callback && (resultEl.innerHTML = 'An error has occurred！');
+    });
+  }
+
+  function doSearch (e) {
+    if (e.isComposing) {
+      return;
+    }
+    if (!e.target.value) {
+      resultEl.innerHTML = oldResultElString;
+      return;
+    }
+    getSearchData(function(data){
+      render(searchFromJson(data, e.target.value));
+    });
+  }
+
+  function goto (url) {
+    window.location.href = url;
+  }
+
+  function gotoSearchResult(e) {
+    e.stopPropagation();
+    var el = e.target;
+
+    if (el.tagName !== 'LI') {
+      el = el.parentNode;
+    }
+    if (el.tagName !== 'LI' || !el.dataset.url) {
+      return;
+    }
+    goto(window.location.origin + el.dataset.url);
+  }
+
+  function onDocKeydown (e) {
+    if (e.keyCode === 27) {
+      hideSearch();
+    }
+  }
+
+  searchEl.addEventListener('click', showSearch);
+  inputEl.addEventListener('input', doSearch);
+  searchWrap.addEventListener('click', hideSearch);
+  searchContent.addEventListener('click', gotoSearchResult);
+  inputEl.addEventListener('compositionend', doSearch);
+  document.addEventListener('keydown', onDocKeydown);
+})();
